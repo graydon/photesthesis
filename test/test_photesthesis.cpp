8065517ce3d6cdaf015822ee26a469b5b9c8bc15
Graydon Hoare <graydon@pobox.com>
2021-02-16 17:00:58 -0800

    revise test for clarity

--- test/test_photesthesis.cpp
+++ test/test_photesthesis.cpp
@@ -9,8 +9,34 @@
 #include <photesthesis/value.h>
 #include <sstream>
 
+// This is the SUT: a miniature calculator with a
+// stack of local symbolic variables.
+class Calculator {
+  std::vector<std::pair<std::string, int64_t>> mVarStack;
+
+public:
+  int64_t getVar(std::string const &var) {
+    for (auto pair = mVarStack.rbegin(); pair != mVarStack.rend(); ++pair) {
+      if (pair->first == var) {
+        return pair->second;
+      }
+    }
+    throw std::runtime_error(std::string("variable ") + var + " not found");
+  }
+  void pushVar(std::string const &var, int64_t val) {
+    mVarStack.emplace_back(var, val);
+  }
+  void popVar() { mVarStack.pop_back(); }
+
+  int64_t add(int64_t a, int64_t b) { return a + b; }
+  int64_t sub(int64_t a, int64_t b) { return a - b; }
+  int64_t mul(int64_t a, int64_t b) { return a * b; }
+};
+
 namespace ph = photesthesis;
 
+// These are symbols used in the grammar that describes abstract test
+// scenarios in terms of arithmetic expressions.
 const ph::RuleName EXPR{"expr"};
 const ph::RuleName ADD{"add"};
 const ph::RuleName SUB{"sub"};
@@ -21,79 +47,9 @@ const ph::ParamName X{"x"};
 const ph::ParamName N{"n"};
 const ph::VarName RES{"res"};
 
-class MyTest : public ph::Test {
-public:
-  MyTest(ph::Grammar const &gram, ph::Corpus &corp)
-      : Test(gram, corp, ph::Symbol("MyTest"), {{{N, EXPR}}}) {}
-
-  void checkRoundtrip(ph::Value const &val) {
-    std::ostringstream oss;
-    oss << val;
-    ph::Value nval;
-    std::istringstream iss(oss.str());
-    iss >> nval;
-    if (!(nval == val)) {
-      std::cout << "deserialized doesn't match:\n  " << val << "\nvs.\n  "
-                << nval << std::endl;
-    }
-  }
-
-  ph::Value eval(std::map<ph::Symbol, ph::Value> env, ph::Value val) {
-    ph::Value a, b, c;
-
-    if (val.match(EXPR, a)) {
-      if (a.match(ADD, b, c)) {
-        ph::Value vb = eval(env, b);
-        ph::Value vc = eval(env, c);
-        int64_t ia{0}, ic{0};
-        if (vb.match(ia) && vc.match(ic)) {
-          return ph::Value::Int64(ia + ic);
-        }
-      }
-      if (a.match(SUB, b, c)) {
-        ph::Value vb = eval(env, b);
-        ph::Value vc = eval(env, c);
-        int64_t ia{0}, ic{0};
-        if (vb.match(ia) && vc.match(ic)) {
-          return ph::Value::Int64(ia - ic);
-        }
-      }
-
-      if (a.match(MUL, b, c)) {
-        ph::Value vb = eval(env, b);
-        ph::Value vc = eval(env, c);
-        int64_t ia{0}, ic{0};
-        if (vb.match(ia) && vc.match(ic)) {
-          return ph::Value::Int64(ia * ic);
-        }
-      }
-
-      if (a.match(LET, X, b, c)) {
-        std::map<ph::Symbol, ph::Value> newEnv = env;
-        newEnv[X] = eval(env, b);
-        return eval(newEnv, c);
-      }
-
-      if (a.match(VAR, X)) {
-        return env[X];
-      }
-
-      if (a.isInt64()) {
-        return a;
-      }
-    }
-    return ph::Value::Int64(0);
-  }
-
-  void run() override {
-    ph::Value val = getParam(N);
-    checkRoundtrip(val);
-    check(RES, eval({}, val));
-  }
-};
-
-int main() {
-
+// This function returns a grammar for abstract tests using the above
+// symbols for terminals and nonterminals.
+ph::Grammar exprGrammar() {
   ph::Grammar gram;
 
   gram.addRule(ADD, {{gram.Int64(0)}, {gram.Ref(EXPR), gram.Ref(EXPR)}});
@@ -113,9 +69,63 @@ int main() {
                       {gram.Ref(LET)},
                       // References to VAR need X as a context symbol.
                       {{gram.Ref(VAR)}, {X}}});
+  return gram;
+}
 
+// This class represents the _interface_ between the the photesthesis testing
+// engine, the abstract test grammar, and the SUT.
+class CalcTest : public ph::Test {
+
+  Calculator mCalc;
+
+public:
+  CalcTest(ph::Grammar const &gram, ph::Corpus &corp)
+      : Test(gram, corp, ph::Symbol("CalcTest"), {{{N, EXPR}}}) {}
+
+  int64_t eval(ph::Value val) {
+    ph::Value a, b, c;
+    int64_t i;
+
+    if (val.match(EXPR, a)) {
+      if (a.match(ADD, b, c)) {
+        return mCalc.add(eval(b), eval(c));
+      }
+      if (a.match(SUB, b, c)) {
+        return mCalc.sub(eval(b), eval(c));
+      }
+
+      if (a.match(MUL, b, c)) {
+        return mCalc.mul(eval(b), eval(c));
+      }
+
+      if (a.match(LET, X, b, c)) {
+        mCalc.pushVar(X.getString(), eval(b));
+        i = eval(c);
+        mCalc.popVar();
+        return i;
+      }
+
+      if (a.match(VAR, X)) {
+        return mCalc.getVar(X.getString());
+      }
+
+      if (a.match(i)) {
+        return i;
+      }
+    }
+    return 0;
+  }
+
+  void run() override {
+    ph::Value val = getParam(N);
+    check(RES, ph::Value::Int64(eval(val)));
+  }
+};
+
+int main() {
   ph::Corpus corp("test.corpus");
-  MyTest test(gram, corp);
+  ph::Grammar gram = exprGrammar();
+  CalcTest test(gram, corp);
   test.seedFromRandomDevice();
   test.administer();
 }
